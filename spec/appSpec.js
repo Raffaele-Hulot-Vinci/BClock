f120016a86102c9608dbd27f3123763dbdc2e538
Raffaele Hulot <raffaele.hulot@student.vinci.be>
2024-11-13 17:59:13 +0100

    relocated appSpec.js into spec from support

---
+++ spec/appSpec.js
@@ -0,0 +1,11 @@
+import { Main } from "../src/app.js"
+
+describe("a bunch of tests for the berlin clock", function() {
+    const main = new Main();
+    const dateZero = new date(0, 0, 0, 0, 0, 0);
+
+    it("bottom line should be - - - - when given 0 minutes", function(){
+        const result = main(dateZero);
+        expect(result).toBe("- - - -");
+    });
+});
