3f8d193db5fbe52f1b34466a24ad06af7e405895
Raffaele Hulot <raffaele.hulot@student.vinci.be>
2024-11-14 13:21:24 +0100

    put variables outside of it

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,28 +1,28 @@
 import { Main } from "../src/app.js"
 
 describe("a bunch of tests for the berlin clock", function() {
+    const main = new Main();
+    const dateZero = new Date(0, 0, 0, 0, 0, 0);
+    const dateSecondsOdd = new Date(1,1,1,1,1,1);
+    const dateSecondsEven = new Date(2,2,2,2,2,2);
     
+
     it("line should be - - - - when given 0 minutes", function(){
-        const main = new Main();
-        const dateZero = new Date(0, 0, 0, 0, 0, 0);
-        
         const result = main.simpleMinutes(dateZero);
         expect(result).toBe("- - - - ");
     });
+
     it("line should return - when seconds are odd", function() {
-        const main = new Main();
-        const dateSecondsOdd = new Date(1,1,1,1,1,1);
         
         const result = main.simpleSeconds(dateSecondsOdd);
         expect(result).toBe("-");
-    }
-    );
+    });
+
     it ("line should return R when seconds are even", function() {
-        const main = new Main();
-        const dateSecondsEven = new Date(2,2,2,2,2,2);
         const result = main.simpleSeconds(dateSecondsEven);
         expect(result).toBe("R");
     });
+    
     it ("line should return the full Berlin Clock display for 00:00:00", function() {
         const result = "J\n----\n----\n------------\n----";
         const main = new Main();
